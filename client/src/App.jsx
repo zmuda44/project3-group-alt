ae3109fab0a98092cf235a2b5a6474600348c6d8
Greg Bailey <gregbailey617@gmail.com>
2024-08-11 20:21:34 -0400

    got it working with apollo frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import Profile from './pages/profile'
 import LandingPage from './gb-front-end/pages/landingPage'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+const httpLink = new createHttpLink({
+  uri: '/graphql',
+});
+
+// took out authentication.  see mod 21 challenge for code
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <div>
+    <ApolloProvider client={client}>
     <LandingPage />
     <Profile />
+    </ApolloProvider>
   </div>
   );
 }
